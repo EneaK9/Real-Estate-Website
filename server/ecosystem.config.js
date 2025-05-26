a6ffbcfdb195ca7f55381cec4ce8022dfc969d0b
Enea Kuca <eneakuca0@gmail.com>
2025-05-26 13:15:20 +0200

    Last update 2.0

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,12 +1,10 @@
 module.exports = {
-  apps: [
-    {
-      name: "real-estate",
-      script: "npm",
-      args: "run dev",
-      env: {
-        NODE_ENV: "development",
-      },
-    },
-  ],
+	apps: [
+		{
+			name: "inventory-management",
+			script: "npm",
+			args: "run dev",
+			env: { NODE_ENV: "development", ENV_VAR1: "environment-variable" },
+		},
+	],
 };
